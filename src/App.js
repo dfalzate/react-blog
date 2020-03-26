00f695f8198fb5ed4d11c33dd51fa9f9306d5864
Diego Alzate <diegoff@gmail.com>
2020-03-26 09:01:53 -0500

    Blog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Posts(props) {
+  return props.posts.map(post => (
+    <p id={post.id} key={post.id} onClick={props.onClick}>
+      {post.title}
+    </p>
+  ));
+}
+class App extends React.Component {
+  state = {
+    posts: [],
+    loading: true,
+    error: null
+  };
+
+  async componentDidMount() {
+    try {
+      const data = await axios({
+        method: 'GET',
+        baseURL: 'http://jsonplaceholder.typicode.com/',
+        url: '/posts',
+        params: {
+          _page: 1,
+          _limit: 25
+        }
+      });
+      this.setState({
+        posts: data.data
+      });
+    } catch (error) {
+      this.setState({ error: 'Something went wrong' });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  clickPost = event => {
+    const idPost = event.target.id;
+    console.log();
+    const post = this.state.posts.filter(
+      post => post.id === parseInt(idPost)
+    )[0];
+    console.log(post);
+  };
+
+  render() {
+    if (this.state.error) return <h1>Loading...</h1>;
+    if (this.state.loading === true) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="App">
+          <h1>Blog</h1>
+          <Posts onClick={this.clickPost} posts={this.state.posts} />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
